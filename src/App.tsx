2f38daf7c0133157f3bb180c00d338d2682a399b
zwonkim <k980912@naver.com>
2023-01-10 18:18:38 +0900

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>hello world</h1>
-    </div>
+    <>
+      <Header/>
+      <Outlet/>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from "./App";
+import Home from './Pages/Home';
+import Products from './Pages/Products';
+
+
+const router = createBrowserRouter([
+  {path: '/', element: <App/>,
+  children : [
+  {index:true, path: '/', element:<Home/>},
+  {path:'/products', element: <Products/>}
+    ]
+  },
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
