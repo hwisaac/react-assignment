b9a4bec8ef16ae86e43c62cf9f8b311e4fd297df
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-01-11 19:20:11 +0900

    homework 03

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 리액트 숙제
+
+- 01: https://github.com/hwisaac/react-assignment/tree/master/src/homework/01
+- 02: https://github.com/hwisaac/react-assignment/tree/master/src/homework/02
+- 03: https://github.com/hwisaac/react-assignment/tree/master/src/homework/03
---
+++ src/homework/03/index.tsx
@@ -0,0 +1,120 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+
+const Homework = () => {
+  return (
+    <>
+      <div className='container'>
+        <div className='item'>
+          <ul className='list'>
+            <li>
+              <img className='logoImg' src='/logo192.png' alt='logo' />
+              <h3 className='subTitle'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </h3>
+            </li>
+            <li>
+              <span className='text'>
+                Ab adipisci, suscipit sint reprehenderit repellat autem cumque
+                fugiat
+                <a href='#' className='link'>
+                  link
+                </a>
+              </span>
+            </li>
+            <li>
+              <span className='text'>
+                eos amet soluta placeat consequatur incidunt illo tempore
+                ducimus, iste labore
+              </span>
+              <a href='#' className='link'>
+                link
+              </a>
+            </li>
+            <li>
+              <span>minus distinctio</span>
+              <a href='#' className='link'>
+                link
+              </a>
+            </li>
+          </ul>
+          <button className='btn'>BUTTON</button>
+        </div>
+        <div className='item'>
+          <ul className='list'>
+            <li>
+              <img className='logoImg' src='/logo192.png' alt='logo' />
+              <h3 className='subTitle'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </h3>
+            </li>
+            <li>
+              <span className='text'>
+                Ab adipisci, suscipit sint reprehenderit repellat autem cumque
+                fugiat
+              </span>
+              <a href='#' className='link'>
+                link
+              </a>
+            </li>
+            <li>
+              <span className='text'>
+                eos amet soluta placeat consequatur incidunt illo tempore
+                ducimus, iste labore
+              </span>
+              <a href='#' className='link'>
+                link
+              </a>
+            </li>
+            <li>
+              <span>minus distinctio</span>
+              <a href='#' className='link'>
+                link
+              </a>
+            </li>
+          </ul>
+          <button className='btn'>BUTTON</button>
+        </div>
+        <div className='item'>
+          <ul className='list'>
+            <li>
+              <img className='logoImg' src='/logo192.png' alt='logo' />
+              <h3 className='subTitle'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </h3>
+            </li>
+            <li>
+              <span className='text'>
+                Ab adipisci, suscipit sint reprehenderit repellat autem cumque
+                fugiat
+              </span>
+              <a href='#' className='link'>
+                link
+              </a>
+            </li>
+            <li>
+              <span className='text'>
+                eos amet soluta placeat consequatur incidunt illo tempore
+                ducimus, iste labore
+              </span>
+              <a href='#' className='link'>
+                link
+              </a>
+            </li>
+            <li>
+              <span>minus distinctio</span>
+              <a href='#' className='link'>
+                link
+              </a>
+            </li>
+          </ul>
+          <button className='btn'>BUTTON</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Homework;
