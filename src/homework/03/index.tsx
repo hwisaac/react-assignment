c419fa90db151a5b4fa86febb793c2b569904797
zwonkim <k980912@naver.com>
2023-01-12 15:06:22 +0900

    modified third assignment

--- src/homework/03/index.tsx
+++ src/homework/03/index.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-// import "./index.css";
 import styled from 'styled-components';
 
 const Homework = () => {
--- src/homework/04/index.tsx
+++ src/homework/04/index.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
 
 const Homework = () => {
   return <>hello</>;
