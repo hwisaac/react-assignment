9acb8509e3e94c0db8bf978927991b6cdce4b0ed
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-01-12 23:22:19 +0900

    homework 04

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - 01: https://github.com/hwisaac/react-assignment/tree/master/src/homework/01
 - 02: https://github.com/hwisaac/react-assignment/tree/master/src/homework/02
 - 03: https://github.com/hwisaac/react-assignment/tree/master/src/homework/03
+- 04: https://github.com/hwisaac/react-assignment/tree/master/src/homework/04
--- src/homework/04/index.tsx
+++ src/homework/04/index.tsx
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
 const Homework = () => {
+  const data = [
+    { title: "사과", price: 1000, isSoldOut: false },
+    { title: "오렌지", price: 1500, isSoldOut: false },
+    { title: "수박", price: 10000, isSoldOut: true },
+    { title: "포도", price: 20000, isSoldOut: false },
+    { title: "바나나", price: 5000, isSoldOut: false },
+    { title: "블루베리", price: 10000, isSoldOut: true },
+  ];
+
   return <>hello</>;
 };
 
