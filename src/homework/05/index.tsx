7a95c38a4f68fa0b340962e2498739ab93769496
hae9 <gorud0929@naver.com>
2023-01-14 00:34:19 +0900

    230114 - homework 5

--- src/homework/05/index.tsx
+++ src/homework/05/index.tsx
@@ -1,26 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 
 interface IFruit {
-  // 작성
+  name: string;
+  price: number;
 }
-
 class Fruits {
-  // 작성
+  fruits;
+  constructor(fruits: IFruit[]) {
+    this.fruits = fruits;
+  }
+  get() {
+    return this.fruits;
+  }
+  set(fruits: IFruit[]) {
+    this.fruits = fruits;
+  }
 }
 
 const oldFruits: IFruit[] = [
-  { name: "apple", price: 1000 },
-  { name: "orange", price: 2000 },
-  { name: "banana", price: 3000 },
-  { name: "melon", price: 4000 },
+  { name: 'apple', price: 1000 },
+  { name: 'orange', price: 2000 },
+  { name: 'banana', price: 3000 },
+  { name: 'melon', price: 4000 },
 ];
 const newFruits: IFruit[] = [
-  { name: "mango", price: 5000 },
-  { name: "kiwi", price: 6000 },
-  { name: "grape", price: 7000 },
-  { name: "water melon", price: 8000 },
+  { name: 'mango', price: 5000 },
+  { name: 'kiwi', price: 6000 },
+  { name: 'grape', price: 7000 },
+  { name: 'water melon', price: 8000 },
 ];
 
 const Homework = () => {
@@ -32,9 +41,21 @@ const Homework = () => {
   return (
     <>
       <h2>old one</h2>
-      {/* 작성하세요 */}
+      <ul>
+        {oldOne.map((oldFruit) => (
+          <li key={oldFruit.name}>
+            {oldFruit.name} / {oldFruit.price}
+          </li>
+        ))}
+      </ul>
       <h2>new one</h2>
-      {/* 작성하세요 */}
+      <ul>
+        {newOne.map((newFruit) => (
+          <li key={newFruit.name}>
+            {newFruit.name} / {newFruit.price}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
