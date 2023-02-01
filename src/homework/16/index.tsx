a4143318195e7136bb1c964c5708089cebb2ec41
zwonkim <k980912@naver.com>
2023-02-01 21:45:13 +0900

    assignment 16

--- src/homework/16/index.tsx
+++ src/homework/16/index.tsx
@@ -1,12 +1,52 @@
-import React, { useState } from "react";
-// import "./tailwind.css";
+import React from "react";
+import Item from './Item';
 
 export function cls(...classnames: string[]) {
   return classnames.join(" ");
 }
 
+const addItemBtn = [
+  'w-10', 
+  'h-10', 
+  'bg-orange-500',
+  'fixed',
+  'bottom-6',
+  'right-6',
+  'rounded-full', 
+  'flex', 
+  'items-center', 
+  'justify-center',
+]
+
+export type Iphone = {
+  title: string;
+  color: string;
+  price: string;
+}
+
+const iphones: Iphone[] = [
+  {title: 'New iPhone 14' , color: 'Black', price: '$95'},
+  {title: 'New iPhone 14' , color: 'Black', price: '$95'},
+  {title: 'New iPhone 14' , color: 'Black', price: '$95'},
+  {title: 'New iPhone 14' , color: 'Black', price: '$95'},
+  {title: 'New iPhone 14' , color: 'Black', price: '$95'},
+  {title: 'New iPhone 14' , color: 'Black', price: '$95'},
+  {title: 'New iPhone 14' , color: 'Black', price: '$95'},
+  {title: 'New iPhone 14' , color: 'Black', price: '$95'},
+  {title: 'New iPhone 14' , color: 'Black', price: '$95'},
+]
+
 const Homework = () => {
-  return <div>Homework 16</div>;
+  return (
+    <div className='flex flex-col p-10'>
+      {iphones.map((iphone, index) => {
+        return <Item key={index} {...iphone}/>
+      })}
+      <div className={cls(...addItemBtn)}>
+        <p className='text-2xl text-white'>+</p>
+      </div>
+    </div>
+  );
 };
 
 export default Homework;
