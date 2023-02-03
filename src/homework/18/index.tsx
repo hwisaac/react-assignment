bbf8084bc8a274af7add16ec19683073d1f5c6e5
Hyeeeein <khi_9866@naver.com>
2023-02-04 06:30:54 +0900

    230203 Complete assignment 18

--- src/homework/18/index.tsx
+++ src/homework/18/index.tsx
@@ -1,9 +1,33 @@
-// import "./tailwind.css";
+import { useState } from "react";
+import { AiFillPlusCircle } from "react-icons/ai";
+import "./tailwind.css";
+import ModalPost from "./ModalPost";
+import PostItem from "./PostItem";
 
 const Homework = () => {
+  const [modal, setModal] = useState(false);
+
+  const onClick = () => {
+    setModal((prev) => !prev);
+  };
+
+  const items = new Array<number>();
+
+  for (let i = 1; i <= 4; i++) {
+    items.push(i);
+  }
+
   return (
     <>
-      <h1>Homework 18</h1>
+      <ul className="flex flex-col items-center p-5">
+        {items.map((i) => (
+          <PostItem key={i} />
+        ))}
+      </ul>
+      <button onClick={onClick} className="fixed bottom-10 right-10">
+        <AiFillPlusCircle size={35} color="#b41bf0" />
+      </button>
+      {modal ? <ModalPost setModal={setModal} /> : null}
     </>
   );
 };
