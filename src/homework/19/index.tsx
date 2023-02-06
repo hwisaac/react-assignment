d8b6d50c5ace55d1dc231ae86f944de431634ddb
hae9 <gorud0929@naver.com>
2023-02-07 00:39:54 +0900

    230207 - homework 19

--- src/homework/19/index.tsx
+++ src/homework/19/index.tsx
@@ -1,32 +1,55 @@
-import React, { useEffect, useState } from "react";
-import LiSubCheckbox from "./LiSubCheckbox";
+import React, { useEffect, useState } from 'react';
+import LiSubCheckbox from './LiSubCheckbox';
 
 const myData = [
   {
-    id: "list1",
-    text: "item1",
+    id: 'list1',
+    text: 'item1',
   },
   {
-    id: "list2",
-    text: "item2",
+    id: 'list2',
+    text: 'item2',
   },
   {
-    id: "list3",
-    text: "item3",
+    id: 'list3',
+    text: 'item3',
   },
 ];
 
 const Homework = () => {
+  const [checkedItems, setCheckedItems] = useState<string[]>([]);
+
+  const handleAllChecks = (event: React.ChangeEvent) => {
+    const target = event.target as HTMLInputElement;
+    if (target.checked) {
+      const tempArray: string[] = [];
+      myData.map((value) => tempArray.push(value.id));
+      setCheckedItems(tempArray);
+    } else {
+      setCheckedItems([]);
+    }
+  };
+
   return (
     <>
       <header>
-        <input type='checkbox' />
+        <input
+          type="checkbox"
+          checked={checkedItems.length === myData.length ? true : false}
+          onChange={handleAllChecks}
+        />
         전체선택
       </header>
       <main>
         <ul>
           {myData.map((item) => (
-            <LiSubCheckbox key={item.id} />
+            <LiSubCheckbox
+              key={item.id}
+              id={item.id}
+              text={item.text}
+              checkedItems={checkedItems}
+              setCheckedItems={setCheckedItems}
+            />
           ))}
         </ul>
       </main>
